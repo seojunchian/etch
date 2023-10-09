922ea658711a3aea494081af880dd8d17fa3bd66
ychilo <seojunchian@gmail.com>
2023-10-09 15:06:50 +0300

    asd

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
-const squareNumber = document.querySelector(".rangeNumber");
-const squareEntry = document.querySelector(".rangeEntry");
+const rangeNumber = document.querySelector(".rangeNumber");
+const range = document.querySelector(".range");
+const square = document.querySelector(".square");
 
+rangeNumber.textContent = `${range.value}x${range.value}`;
+
+const rangeChange = range.addEventListener("click", () => {
+    rangeNumber.textContent = `${range.value}x${range.value}`;
+    createSquare();
+})
+
+function createSquare() {
+    for(let i=0; i<range.value-1; i++){
+        const div = document.createElement("div");
+        div.style.width = 410 / range.value;
+        div.style.height = 410 / range.value;
+        div.style.backgroundColor = "red";
+        square.appendChild(div);
+    }
+}
